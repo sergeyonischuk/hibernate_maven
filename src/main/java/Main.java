7c553a3d9cd03f471217cfcf7e8308c0effe0842
sergeyonischuk <sergey.onischuk@ukr.net>
2023-03-13 17:40:01 +0200

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,24 +8,10 @@ import utils.HibernateUtil;
 
 public class Main {
     public static void main(String[] args) {
-//Create Java object
-        Planet p = new Planet();
-        p.setId("MIN");
-        p.setName("Minerva 93");
-
-//Persist object
-//        Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
-//        Transaction transaction = session.beginTransaction();
-//        session.persist(p);
-//        transaction.commit();
-//        session.close();
         SessionFactory sessionFactory = new Configuration()
                 .addAnnotatedClass(Planet.class)
                 .buildSessionFactory();
         Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-//        session.persist(p);
-//        transaction.commit();
         session.close();
 
     }
